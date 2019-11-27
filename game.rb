706abfa76554e1e8890a45ff606f1f4269cf4b77
John <john@mindhiveinteractive.com>
2019-11-26 21:38:17 -0600

    mass sim

--- game.rb
+++ game.rb
@@ -16,9 +16,9 @@ class Game
     toss_count += 1
     # if x is 7 or 11, instant win
     if [7, 11].include?(x)
-      outcome = 1
+      outcome = "COME OUT WIN"
     elsif [2, 3, 12].include?(x)
-      outcome = 0
+      outcome = "COME OUT LOSS"
     else
       toss = DIE.sample + DIE.sample
       p "toss: #{toss}"
@@ -33,16 +33,16 @@ class Game
       end
       # if sum is x then win
       # if sum is 7 then lose
-      outcome = toss == x ? 1 : 0
+      outcome = toss == x ? "HIT" : "7 OUT"
     end
     p "outcome: #{outcome} toss: #{x} toss_count: #{toss_count}"
 
-    if outcome != 0
+    if outcome != "7 OUT"
       run(toss_count)
     else
-      outcomes << toss_count
+      @outcomes << toss_count
       p "rolls: #{rolls.join(',')}"
-      p "toss count: #{outcomes.join(',')}"
+      p "toss count: #{@outcomes.join(',')}"
     end
   end
 end
--- sim.rb
+++ sim.rb
@@ -1,9 +1,22 @@
 require './game.rb'
 class Sim
-  def self.run
+  attr_accessor :outcomes
+
+  def initialize
+    @outcomes = []
+  end
+
+  def run
     g = Game.new
     g.run
+    g.outcomes.each do |outcome|
+      @outcomes << outcome
+    end
   end
 end
 
-Sim.run
+s = Sim.new
+100.times.each do
+  s.run
+end
+p "outcomes: #{s.outcomes.join(',')}"
