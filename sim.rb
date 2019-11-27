a5c02b2f4d17ab48257e4d7082e40c4b2a8f9ccd
John <john@mindhiveinteractive.com>
2019-11-26 21:51:08 -0600

    add summary stats

--- sim.rb
+++ sim.rb
@@ -15,8 +15,32 @@ class Sim
   end
 end
 
+def mean(arr)
+  arr.sum.fdiv(arr.size)
+end
+
+def median(array, already_sorted=false)
+  return nil if array.empty?
+  array = array.sort unless already_sorted
+  m_pos = array.size / 2
+  return array.size % 2 == 1 ? array[m_pos] : mean(array[m_pos-1..m_pos])
+end
+
+def modes(array, find_all=true)
+  histogram = array.inject(Hash.new(0)) { |h, n| h[n] += 1; h }
+  modes = nil
+  histogram.each_pair do |item, times|
+    modes << item if modes && times == modes[0] and find_all
+    modes = [times, item] if (!modes && times>1) or (modes && times>modes[0])
+  end
+  return modes ? modes[1...modes.size] : modes
+end
+
 s = Sim.new
-100.times.each do
+1000.times.each do
   s.run
 end
 p "outcomes: #{s.outcomes.join(',')}"
+p "median: #{median(s.outcomes)}"
+p "mean: #{mean(s.outcomes)}"
+p "mode: #{modes(s.outcomes)}"
